c619f1a87aa7eb90723d773d11d842d8c8b057f3
hamtolchu <seungjaey@gmail.com>
2023-04-04 18:45:00 +0900

    add .well-known apple json

--- next.config.js
+++ next.config.js
@@ -21,6 +21,15 @@ const nextConfig = {
           },
         ],
       },
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
     ];
   },
 };
