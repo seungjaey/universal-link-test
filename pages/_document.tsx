076da6d52a46c3aee0f1bacdae40bc0b4f83744b
hamtolchu <seungjaey@gmail.com>
2023-04-04 16:30:37 +0900

    add android link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <link rel="alternate" href="android-app://com.dbs.kurly.m2/https/www.stg.kurly.com" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
