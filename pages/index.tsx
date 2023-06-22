eeefa4e24271aa09a91c0815073f9ac71e3730a2
hamtolchu <seungjaey@gmail.com>
2023-06-22 14:52:11 +0900

    테스트 링크 버튼 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
+import Link from 'next/link';
+
+const style = { marginBottom: '16px' };
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ul>
+        <li style={style}>
+          <Link href="/goods/5063110" passHref prefetch={false}>
+            [1] 상품 상세 [5063110] 로 이동하기
+          </Link>
+        </li>
+        <li style={style}>
+          <a href="/goods/5063110">[2] 상품 상세 [5063110] 로 이동하기</a>
+        </li>
+        <li style={style}>
+          <a href="https://universal-link-test-beryl.vercel.app/goods/5063110">[3] 상품 상세 [5063110] 로 이동하기</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
