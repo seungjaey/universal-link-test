978a60683e9f5d7b8a44de039873e6aab8221857
seungjaey <seungjaey@gmail.com>
2023-07-12 15:39:11 +0900

    test

--- pages/redirect/goods/[productCode].tsx
+++ pages/redirect/goods/[productCode].tsx
@@ -1,22 +1,10 @@
 import { useRouter } from 'next/router';
-import {GetServerSideProps} from "next";
 
 const DynamicPage = () => {
-    const { query } = useRouter();
-
+    const { query, replace } = useRouter();
+    const { productCode } = query;
+    replace(`/goods/${productCode}`)
     return null;
 };
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const { req } = ctx;
-    console.log('check');
-    return {
-        props: {},
-        redirect: {
-            statusCode: 307,
-            destination: `/goods/${ctx?.params?.productCode}`,
-        }
-    }
-}
 
 export default DynamicPage;
