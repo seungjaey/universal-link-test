2a4e10e6b28243e9279c14164b5e2dc96d26c018
seungjaey <seungjaey@gmail.com>
2023-07-12 15:41:39 +0900

    test2

--- pages/redirect/goods/[productCode].tsx
+++ pages/redirect/goods/[productCode].tsx
@@ -1,8 +1,11 @@
 import { useRouter } from 'next/router';
 
 const DynamicPage = () => {
-    const { query, replace } = useRouter();
+    const { query, isReady, replace } = useRouter();
     const { productCode } = query;
+    if (!isReady) {
+        return null;
+    }
     replace(`/goods/${productCode}`)
     return null;
 };
